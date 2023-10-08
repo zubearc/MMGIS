f8bbbb3608965949043c806945abf84113e945da
zubearc <zubearc@users.noreply.github.com>
2023-10-08 22:35:11 +0000

    deploy: 0bf1349f1decdc627e7b85a97dd6630cf9593dbe

---
+++ documentation/pages/swaggers/swaggerJS.js
@@ -0,0 +1,1 @@
+document.getElementsByTagName("title")[0].innerHTML="MMGIS API";const favicons=document.querySelectorAll('link[rel="icon"]');for(let e=0;e<favicons.length;e++)favicons[e].setAttribute("href","public/images/logos/logo.png");
